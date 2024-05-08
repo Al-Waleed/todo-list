d5aaaafb9b837606905fbd2a8ecd0a1dd4613337
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-05-08 13:00:18 +0300

    Import functions from tasks.js and projects.js && create a function that adds a task inside of a project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import {createTask, displayTask, getTaskInfo, tasks } from "./tasks";
+import { createProject, displayAllProjects, displayProject, projects } from "./projects";
+
+
+function addTaskToProject(){
+    const task = prompt("choose task to add to project")
+    const project = prompt("choose project to add the task to")
+    const taskName = tasks[task].title;
+    console.log(taskName);
+    projects[project][taskName] = tasks[task];
+
+    console.log(projects)
+}
+
+createTask();
+createTask();
+createProject();
+addTaskToProject(); 
+addTaskToProject(); 
